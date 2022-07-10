6243c0c5919034ff2501de540f2606bb06ab6ba7
Duong Pham <duong@itads.net>
2022-07-10 18:53:51 +0700

    Update signup

--- app/src/main/java/com/cuongpq/basemvvm/ui/main/fragment/signup/SignupFragment.kt
+++ app/src/main/java/com/cuongpq/basemvvm/ui/main/fragment/signup/SignupFragment.kt
@@ -1,11 +1,13 @@
 package com.cuongpq.basemvvm.ui.main.fragment.signup
 
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
 import com.cuongpq.basemvvm.R
 import com.cuongpq.basemvvm.databinding.FragmentSignupBinding
 import com.cuongpq.basemvvm.ui.base.fragment.BaseMvvmFragment
 import com.cuongpq.basemvvm.ui.base.viewmodel.BaseViewModel
+import com.cuongpq.basemvvm.ui.main.MainActivity
 import com.cuongpq.basemvvm.ui.main.fragment.signin.SigninViewModel
 
 class SignupFragment : BaseMvvmFragment<SignupCallBack,SignupViewModel>(),SignupCallBack{
@@ -23,26 +25,48 @@ class SignupFragment : BaseMvvmFragment<SignupCallBack,SignupViewModel>(),Signup
     override fun initComponents() {
         getBindingData().signupViewModel = mModel
         startSignup()
-//        mModel.uiEventLiveData.observe(this){
-//            when(it){
-//                BaseViewModel.FINISH_ACTIVITY -> finishActivity()
+        mModel.uiEventLiveData.observe(this){
+            when(it){
+                BaseViewModel.FINISH_ACTIVITY -> finishActivity()
+                SignupViewModel.NAV_GET_DATA_FROM_UI_AND_REGISTER -> startSignup()
+                SignupViewModel.NAV_REGISTER_SUCCESS -> onSignupSuccess()
+                SignupViewModel.NAV_REGISTER_ERROR -> onSignupError()
 //                SignupViewModel.START_SIGNUP -> startSignup()
-//            }
-//        }
+            }
+        }
 
     }
 
     private fun startSignup() {
         getBindingData().btnSignup.setOnClickListener {
-
             val email = getBindingData().edtEmail.text.toString().trim()
             val passwrod = getBindingData().edtPassword.text.toString().trim()
             val user = getBindingData().edtUser.text.toString().trim()
             val confirmPassword = getBindingData().edtConfirmPassword.text.toString().trim()
             val phone = getBindingData().edtPhone.text.toString().trim()
-            mModel.onSignup(email, user, passwrod, confirmPassword, phone)
+            if (email.isEmpty() || passwrod.isEmpty() || user.isEmpty() || confirmPassword.isEmpty() || phone.isEmpty()){
+                getBindingData().tvError.text = "Field can not null"
+                return@setOnClickListener;
+            }
+            getBindingData().tvError.text = ""
+            mModel.emailAddress = email
+            mModel.username = user
+            mModel.password = passwrod
+            mModel.confirmPassword = confirmPassword
+            mModel.phoneNumber = phone
+            mModel.onSignup()
 
        }
+    }
+
+    private fun onSignupSuccess(){
+        val intent = Intent(context, MainActivity::class.java)
+        context?.startActivity(intent)
+        showMessage("Signup Success")
+    }
+
+    private fun onSignupError(){
+        showMessage("Signup Error")
     }
 
     override fun getBindingData() = mBinding as FragmentSignupBinding
--- app/src/main/java/com/cuongpq/basemvvm/ui/main/fragment/signup/SignupViewModel.kt
+++ app/src/main/java/com/cuongpq/basemvvm/ui/main/fragment/signup/SignupViewModel.kt
@@ -1,11 +1,13 @@
 package com.cuongpq.basemvvm.ui.main.fragment.signup
 
 import android.app.Application
+import android.text.TextUtils
 import androidx.lifecycle.MutableLiveData
 import com.cuongpq.basemvvm.data.local.AppDatabase
 import com.cuongpq.basemvvm.data.remote.InteractCommon
 import com.cuongpq.basemvvm.ui.base.viewmodel.BaseViewModel
 import com.cuongpq.basemvvm.ui.main.repository.AuthenticationRepository
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import java.util.concurrent.Executor
 import javax.inject.Inject
@@ -18,21 +20,40 @@ class SignupViewModel @Inject constructor(
 ) : BaseViewModel<SignupCallBack>(appDatabase,interactCommon,scheduler){
     private var repository : AuthenticationRepository?=null
     private var userData: MutableLiveData<FirebaseUser>? = null
-    private var application : Application = Application()
+    private var firebaseUserMutableLiveData: MutableLiveData<FirebaseUser>? = null
+//    private var application : Application = Application()
+    private var auth: FirebaseAuth?=null
+    lateinit var emailAddress: String
+    lateinit var username: String
+    lateinit var password: String
+    lateinit var confirmPassword: String
+    lateinit var phoneNumber: String
 
     companion object {
         const val START_SIGNUP = 1004
+        const val NAV_GET_DATA_FROM_UI_AND_REGISTER = 1005
+        const val NAV_REGISTER_SUCCESS = 1006
+        const val NAV_REGISTER_ERROR = 1007
     }
     init {
-        repository = AuthenticationRepository(application)
-        userData = repository!!.getFirebaseUserMutableLiveData()
+        auth = FirebaseAuth.getInstance()
+        if (auth!!.currentUser != null) {
+            firebaseUserMutableLiveData!!.postValue(auth!!.currentUser)
+        }
+//        repository = AuthenticationRepository(application)
+//        userData = repository!!.getFirebaseUserMutableLiveData()
     }
 
-    fun onSignup(email : String ,user:String, password : String,confirmPassword:String,phone : String){
-//        uiEventLiveData.value= START_SIGNUP
-
-        repository!!.onregister(email,user,password,confirmPassword,phone)
+    fun onSignup(){
+        auth!!.createUserWithEmailAndPassword(emailAddress, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                uiEventLiveData.value = NAV_REGISTER_SUCCESS
+            } else {
+                uiEventLiveData.value = NAV_REGISTER_ERROR
+            }
+        }
     }
-
-
+    fun performSignIn(){
+        uiEventLiveData.value = NAV_GET_DATA_FROM_UI_AND_REGISTER
+    }
 }
